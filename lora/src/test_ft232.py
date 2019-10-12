8dd4c7f0d0f8193a5cd62ccacf09dff99342a561
Daniel Connolly <djconnolly27@gmail.com>
2019-10-11 21:04:56 -0400

    Documents spi chip setup

--- lora/src/test_ft232.py
+++ lora/src/test_ft232.py
@@ -1,12 +1,18 @@
+import usb.core
 from ftdi_ft232.spi import SPI
+import ftdi_ft232.board as board
+import adafruit_rfm9x
+import busio
+from digitalio import DigitalInOut, Direction, Pull
 # import time
 # import serial
-import usb.core
-dev=usb.core.find(idVendor=0x0403,idProduct=0x6014)
+# dev=usb.core.find(idVendor=0x0403,idProduct=0x6014)
 # dev.set_configuration()
-print(dev)
+# print(dev)
 
-test = SPI()
+# test = SPI()
+# slave = test._spi.get_port(cs=0, freq=12E6, mode=0)
+# slave.write(b'hello world!')
 # test.write(0x03)
 # print(test.readinto(0x03))
 # from pyftdi.usbtools import UsbTools
@@ -14,3 +20,13 @@ test = SPI()
 # usb = UsbTools.find_all()
 # print(usb)
 
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+CS = DigitalInOut(board.CS)
+RESET = DigitalInOut(board.RESET)
+
+while True:
+    try:
+        rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+        print('RFM9x: Detected')
+    except RuntimeError:
+        print('RFM9x: ERROR')
