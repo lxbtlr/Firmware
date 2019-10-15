707a893cc65fda6a24e95abca3bedbe6e56fadc0
Alex Butler <45553575+lxbtlr@users.noreply.github.com>
2019-10-14 21:18:24 -0400

    Add files via upload
    BNO055 yaw, pitch, and roll serial readout code

---
+++ IMU/YawRollPitch/YawRollPitch.ino
@@ -0,0 +1,56 @@
+//---- Included Libraries ----//
+#include <Wire.h>                           // I²C library
+#include <math.h>                           // trig functions
+#include <Adafruit_Sensor.h>                // Base library for sensors
+#include <Adafruit_BNO055.h>                // BNO055 specific library
+#include <utility/imumaths.h>               // Vector, Matrix, and IMUMath library
+//#include <servo.h>                        // Standard Servo library
+#include <XLR8Float.h>                      // XLR8 accelerated floating point math
+
+#define BNO055_SAMPLERATE_DELAY_MS (50)     // Set pause between samples
+
+//---- Variable Declaration ----//
+
+boolean debug = true;                       // true/false = extra/no information over serial
+
+                        // Digital pin for pitch
+
+float roll, pitch, yaw;                     // Variable to hold roll, pitch, yaw information
+
+Adafruit_BNO055 bno = Adafruit_BNO055();    // Use object bno to hold information
+
+
+void setup(void) {
+
+
+  Serial.begin(115200);                     // Create serial connection at 115,000 Baud
+
+  if (!bno.begin())                         // Attempt communication with sensor
+  {
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+  }
+
+  delay(100);                               // Wait 0.1 seconds to allow it to initialize
+  bno.setExtCrystalUse(true);               // Tell sensor to use external crystal
+}
+
+//---- Main Program Loop ----//
+void loop() {
+
+  //---- Request Euler Angles from Sensor ----//
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+
+  if (debug) {                             // If debug is true, send information over serial
+    Serial.print("Measured Euler Roll-Pitch-Yaw");
+    Serial.print("\t   yaw: "); Serial.print(euler.x()); Serial.print("\t");
+    Serial.print("\t pitch: "); Serial.print(euler.z()); Serial.print("\t");
+    Serial.print("\t  roll: "); Serial.print(euler.y()); Serial.println();
+  }
+  
+
+  
+  // If debug is true, send information over serial
+  
+
+  delay(BNO055_SAMPLERATE_DELAY_MS);       // Wait before rerunning loop
+}
